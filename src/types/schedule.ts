9c981108b4effba8b6aedff0b5c7f4ef9946ddc4
Matias Rozenblum <37559797+matiasrozenblum@users.noreply.github.com>
2025-04-21 00:46:50 -0300

    Fix/better requests (#60)
    * better requests
    
    * updated changelog

--- src/types/schedule.ts
+++ src/types/schedule.ts
@@ -1,14 +1,20 @@
-import { Program } from './program';
-import { Channel } from './channel';
-
 export interface Schedule {
   id: number;
   day_of_week: string;
   start_time: string;
   end_time: string;
-  program: Program & { 
-    channel: Channel;
-    is_live: boolean;
+  program: {
+    id: number;
+    name: string;
+    logo_url: string | null;
+    description: string | null;
     stream_url: string | null;
+    is_live: boolean;
+    panelists: { id: string; name: string }[];
+    channel: {
+      id: number;
+      name: string;
+      logo_url: string | null;
+    };
   };
 }
