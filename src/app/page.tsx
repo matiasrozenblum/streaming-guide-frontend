b1aa999df65a67e9b06dca907323dbd25e26c2ee
matiasrozenblum <rozenblummatias@gmail.com>
2025-05-03 00:07:05 -0300

    api url fallback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,18 @@ export const revalidate = 60;
 import { ChannelWithSchedules } from '@/types/channel';
 import HomeClient from '@/components/HomeClient';
 
+
 export default async function Page() {
   // Calcula el día de la semana en inglés en minúsculas
   const today = new Date()
     .toLocaleString('en-US', { weekday: 'long' })
     .toLowerCase();
 
+  const url = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+
   // Fetch inicial en el servidor, con revalidación ISR cada 60s
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/channels/with-schedules?day=${today}`,
+    `${url}/channels/with-schedules?day=${today}`,
     { next: { revalidate: 60 } }
   );
   const initialData: ChannelWithSchedules[] = await res.json();
