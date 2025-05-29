fa20f849e1a81771683dbbb21efcea5f81c4986f
matiasrozenblum <rozenblummatias@gmail.com>
2025-05-28 23:46:07 -0300

    only show panelists on programs longer than 1 hour

--- src/components/ProgramBlock.tsx
+++ src/components/ProgramBlock.tsx
@@ -443,7 +443,7 @@ export const ProgramBlock: React.FC<Props> = ({
                     >
                       {name.toUpperCase()}
                     </Typography>
-                    {panelists && panelists.length > 0 && (
+                    {panelists && panelists.length > 0 && widthPx > 120 && (
                       <Typography
                         variant="caption"
                         sx={{
