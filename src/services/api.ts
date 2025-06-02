a2371f22b65b62d012bd59e4338bb90b30239c26
matiasrozenblum <rozenblummatias@gmail.com>
2025-06-02 00:28:44 -0300

    testing

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { signOut } from "next-auth/react";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
-
 export const api = axios.create({
-  baseURL: API_URL,
+  baseURL: "/api",
 });
 
 api.interceptors.response.use(
